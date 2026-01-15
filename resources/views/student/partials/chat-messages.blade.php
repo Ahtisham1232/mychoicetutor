5d6e52b1b629a4f04cd543bb59ef44b5b7c4995c
Ahtisham1232 <ahishamullhaq1@gmail.com>
2026-01-15 18:43:40 +0000

    updated

---
+++ resources/views/student/partials/chat-messages.blade.php
@@ -0,0 +1,61 @@
+<div class="chat-messages p-4">
+    @if (empty($messages))
+        <div class="chat-message-center pb-4">
+            Please select anyone from the list to start chat
+        </div>
+    @else
+        @foreach ($messages as $message)
+            @if ($message->from_id === session('userid')->id && $message->from_role_id === session('userid')->role_id)
+                {{-- Outgoing message --}}
+                <div class="chat-message-right pb-4">
+                    <div>
+                        @if (empty($studentProfile->profile_pic ?? ''))
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                class="rounded-circle mr-1" alt="You" width="40" height="40">
+                        @else
+                            <img src="{{ url('images/students/profilepics') }}/{{ $studentProfile->profile_pic }}"
+                                class="rounded-circle mr-1" alt="You" width="40" height="40">
+                        @endif
+                        <div class="text-muted small text-nowrap mt-2">
+                            {{ $message->created_at }}
+                        </div>
+                    </div>
+                    <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
+                        <div class="font-weight-bold mb-1">You</div>
+                        {{ $message->body }}
+                    </div>
+                </div>
+            @else
+                {{-- Incoming message --}}
+                <div class="chat-message-left pb-4">
+                    <div>
+                        @if (isset($header) && $header)
+                            @if ($header->role_id == 2)
+                                @if (empty($header->profile_pic))
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                        class="rounded-circle mr-1" alt="{{ $header->name }}" width="40" height="40">
+                                @else
+                                    <img src="{{ url('images/tutors/profilepics') }}/{{ $header->profile_pic }}"
+                                        class="rounded-circle mr-1" alt="{{ $header->name }}" width="40" height="40">
+                                @endif
+                            @elseif ($header->role_id == 1)
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                    class="rounded-circle mr-1" alt="{{ $header->name }}" width="40" height="40">
+                            @endif
+                        @else
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                class="rounded-circle mr-1" alt="Sender" width="40" height="40">
+                        @endif
+                        <div class="text-muted small text-nowrap mt-2">
+                            {{ $message->created_at }}
+                        </div>
+                    </div>
+                    <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
+                        <div class="font-weight-bold mb-1">{{ isset($header) && $header ? $header->name : 'Sender' }}</div>
+                        {{ $message->body }}
+                    </div>
+                </div>
+            @endif
+        @endforeach
+    @endif
+</div>
